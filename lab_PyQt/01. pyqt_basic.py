0df5bce970138c24e407be67047caea169fd57ee
flsbnus <81967132+flsbnus@users.noreply.github.com>
2021-07-07 20:40:46 +0900

    PyQtTest1

---
+++ lab_PyQt/01. pyqt_basic.py
@@ -0,0 +1,19 @@
+#PyQt 기본요소
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        #창 크기 고정
+        self.setFixedSize(1024,768)
+        #창 제목 설정
+        self.setWindowTitle('GA Mario')
+        #창띄우기
+        self.show()
+  #직접실행할때만 실행
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    window=MyApp()
+    sys.exit(app.exec_())
